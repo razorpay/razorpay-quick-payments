353547b527c8f6f16128ed1210d82abb6c54a0fa
Yash Tripathi <yash.tripathi@razorpay.com>
2022-07-04 18:21:10 +0530

    ver bump

--- razorpay-quick-payments.php
+++ razorpay-quick-payments.php
@@ -4,7 +4,7 @@
  * Plugin Name: Razorpay Quick Payments
  * Plugin URI: https://github.com/razorpay/razorpay-quick-payments
  * Description: Quick Payments for Wordpress, by Razorpay.
- * Version: 1.2.5
+ * Version: 1.2.6
  * Author: Team Razorpay
  * Author URI: https://razorpay.com/about/
  * License: GPL2
